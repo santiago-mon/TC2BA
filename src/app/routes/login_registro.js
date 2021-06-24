c7a345c880da6cf605d18d5af78ba40c57e83f83
santirodrigmoca <santirodrigmoca@outlook.com>
2021-06-24 16:16:27 -0500

    Try login 001

--- src/app/routes/login_registro.js
+++ src/app/routes/login_registro.js
@@ -4,7 +4,18 @@ const connection = require('../../config/db');
 
 module.exports = app => {
    app.get('/', (req,res) => {
-        res.render('../views/index.ejs');
+       if (req.session.loggedin) {
+
+       res.render ('../views/index.ejs', {
+           login : true,
+           name : req.session.name
+            });
+        } else {
+           res.render('../views/index.ejs', {
+               login: false,
+               name: 'Por favor Inicie sesión'
+           });
+       }
     })
 
     app.get('/login', (req,res) => {
@@ -17,6 +28,12 @@ module.exports = app => {
 
     app.get('/canasta', (req,res) => {
         res.render('../views/canasta.ejs');
+    })
+
+    app.get('/logout', (req,res) => {
+        req.session.destroy(() => {
+            res.redirect('/');
+        })
     })
 
     //Solicitudes POST en el registro
@@ -47,6 +64,44 @@ module.exports = app => {
                 })
             }
         })
+    })
+
+    //Solicitud POST de login
+    app.post('/auth', async (req,res) => {
+        const {correo,pass} = req.body;
+        let passwordHaash = await bcryptjs.hash(pass, 8);
+
+        if(correo && pass){
+            connection.query('SELECT * FROM users WHERE correo = ?', [correo], async (err, results) => {
+                console.log(results);
+                if (results.length === 0 || !(await bcryptjs.compare(pass, results[0].pass))){
+
+                    res.render('../views/login.ejs', {
+                        //login incorrecto sw2
+                        alert:true,
+                        alertTitle:'Error',
+                        alertMessage:'Correo y/o contraseña incorrectas',
+                        alertIcon:'error',
+                        showConfirmButton:true,
+                        timer:6000,
+                        ruta:'login'
+                    });
+                } else {
+                    req.session.loggedin = true;
+                    req.session.name = results[0].name;
+                    res.render('../views/login.ejs', {
+                        //login correcto sw2
+                        alert:true,
+                        alertTitle:'Conexión exitosa',
+                        alertMessage:'Login Correcto',
+                        alertIcon:'success',
+                        showConfirmButton:false,
+                        timer:1500,
+                        ruta:''
+                    });
+                }
+            })
+        }
     })
 
 
